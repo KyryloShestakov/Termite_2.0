ae854948116aef8dc23b0a3f23b707b9f4e84af7
Kyrylo Shestakov <pochtash100@gmail.com>
2024-11-25 08:30:35 +0100

    Added basic TCP server with client request handling
    - Implemented `TcpServer` class to manage connections using `TcpListener`.
    - Added asynchronous `StartAsync` method to start the server and handle clients.
    - Implemented `ProcessClientAsync` method to manage client connections and process requests.
    - Added logging (`Logger.Log`) at key points in server operations.
    - Integrated `RequestExecutor` to handle incoming requests via `TcpRequest`.
    - Included graceful client disconnection and server shutdown using the `Stop` method.

--- Server/TcpServer.cs
+++ Server/TcpServer.cs
@@ -1,14 +1,19 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using NetworkLibrary.Utilities;
+using Server.Executors;
+using Server.Requests;
+using Utilities;
 
-namespace NetworkLibrary.Networking.Server;
+
+namespace Server;
 
 public class TcpServer
 {
     private TcpListener _tcpListener;
     private bool _isRunning;
+    
+    RequestExecutor _requestExecutor;
 
     public TcpServer()
     {
@@ -19,11 +24,11 @@ public class TcpServer
     {
         _tcpListener.Start();
         _isRunning = true;
-        Logger.Log("Server started");
+        Logger.Log("Server started...", LogLevel.Information, Source.Server);
         while (_isRunning)
         {
             TcpClient tcpClient = await _tcpListener.AcceptTcpClientAsync();
-            Logger.Log(tcpClient);
+            Logger.Log(tcpClient, LogLevel.Information, Source.Server);
 
             _ = ProcessClientAsync(tcpClient);
 
@@ -33,10 +38,10 @@ public class TcpServer
 
     private async Task ProcessClientAsync(TcpClient tcpClient)
     {
-        Logger.Log("Processing client started");
+        Logger.Log("Processing client started",LogLevel.Information, Source.Server);
         try
         {
-            if (!tcpClient.Connected) Logger.Log("Client is not connected");
+            if (!tcpClient.Connected) Logger.Log("Client is not connected", LogLevel.Information, Source.Server);
             TcpHandler handler = new TcpHandler(tcpClient);
 
             while (!handler.IsDisposed)
@@ -44,33 +49,34 @@ public class TcpServer
                 try
                 {
                     TcpRequest request = await handler.AwaitRequestAsync();
-                    Logger.Log(request.ToString());
+                    Logger.Log($"Sender is {tcpClient.Client.RemoteEndPoint} | {request.ToString()}", LogLevel.Information, Source.Server);
                     
                     await ProcessRequestAsync(tcpClient, handler, request);
                 }
                 catch (Exception ex)
                 {
-                    Logger.Log($"Error while processing request: {ex.Message}");
+                    Logger.Log($"Error while processing request: {ex.Message}", LogLevel.Error, Source.Server);
                     break;
                 }
             }
 
-            Logger.Log("Processing cliet finished ");
+            Logger.Log("Processing client finished ", LogLevel.Information, Source.Server);
         }
-        catch (Exception ex){ Logger.Log($"Error in ProcessClientAsync {ex.Message}");}
-        finally{tcpClient.Close(); Logger.Log("Client connection closed");}
+        catch (Exception ex){ Logger.Log($"Error in ProcessClientAsync {ex.Message}", LogLevel.Error, Source.Server); ;}
+        finally{tcpClient.Close(); Logger.Log("Client connection closed", LogLevel.Information, Source.Server);}
     }
 
     private async Task ProcessRequestAsync(TcpClient tcpClient, TcpHandler handler, TcpRequest request)
     {
-        
+        Logger.Log("Processing request started", LogLevel.Information, Source.Server);
+        _requestExecutor = new RequestExecutor(tcpClient, handler, request);
     }
     
     public void Stop()
     {
         _isRunning = false;
         _tcpListener.Stop();
-        Logger.Log("Server stopped");
+        Logger.Log("Server stopped", LogLevel.Information, Source.Server);
     }
 
     
