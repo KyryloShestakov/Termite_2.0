5b37db91da801a5701d4dab3b06d874576450375
Kyrylo Shestakov <pochtash100@gmail.com>
2025-03-17 16:34:39 +0100

    Added comments

--- Ter-Protocol-Lib/Requests/RequestSerializer.cs
+++ Ter-Protocol-Lib/Requests/RequestSerializer.cs
@@ -3,32 +3,59 @@ using System.Text.Json;
 
 namespace Ter_Protocol_Lib;
 
+/// <summary>
+/// The RequestSerializer class handles dynamic deserialization of JSON data into objects based on message type.
+/// It uses reflection to find types that implement IRequest and maps them to the TerMessageType enumeration.
+/// </summary>
 public static class RequestSerializer
 {
+    // Dictionary to store the mapping between TerMessageType and the corresponding request type
     private static readonly Dictionary<TerMessageType, Type> RequestTypes = new();
 
+    /// <summary>
+    /// Static constructor that initializes the RequestTypes dictionary by scanning the current assembly for types 
+    /// that implement IRequest and mapping them to the TerMessageType enum based on the class names.
+    /// </summary>
     static RequestSerializer()
     {
+        // Get all types from the current assembly
         var requestTypes = Assembly.GetExecutingAssembly()
             .GetTypes()
+            // Filter types that implement IRequest and are not interfaces or abstract
             .Where(t => typeof(IRequest).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
 
+        // Loop through the found types
         foreach (var type in requestTypes)
         {
+            // Convert class name to corresponding TerMessageType by removing "Request" suffix
             var enumName = type.Name.Replace("Request", "");
+            
+            // Try to parse the class name into a TerMessageType enum value
             if (Enum.TryParse(enumName, out TerMessageType messageType))
             {
+                // Map the message type to the corresponding request type
                 RequestTypes[messageType] = type;
             }
         }
     }
 
+    /// <summary>
+    /// Deserializes the given JSON payload into the corresponding request type based on the provided message type.
+    /// </summary>
+    /// <param name="messageType">The type of the message to deserialize.</param>
+    /// <param name="payload">The JSON string representing the data to be deserialized.</param>
+    /// <returns>The deserialized object of the corresponding type.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the message type is unknown.</exception>
     public static object? DeserializeData(TerMessageType messageType, string payload)
     {
+        // Check if the message type exists in the dictionary
         if (RequestTypes.TryGetValue(messageType, out var requestType))
         {
+            // Deserialize the JSON payload into the appropriate object type
             return JsonSerializer.Deserialize(payload, requestType);
         }
+        
+        // If the message type is unknown, throw an exception
         throw new InvalidOperationException($"Unknown request type: {messageType}");
     }
 }
